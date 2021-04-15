c78aa547a971c34d77ee0ee18826157d78f331c7
Riley Butterfield <rileybutterfield1@gmail.com>
2021-04-15 14:31:14 -0400

    primary functionality

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# Getting Started with Create React App
+# Riley's One Drop Homework Assignment
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Assignment Instructions
+
+You should budget an hour of coding time. We realize that is a tight constraint but we're trying to be respectful of your time while still allowing you to produce enough code for us to make a fair assessment of your skills. We'd rather review high-quality incomplete work than rushed but complete work.
+
+URL: https://onedrop.today/products.json
+
+- Use JavaScript to fetch the results from the URL above. There should be an array named `products` and inside each product, another array named `variants`.
+- Using the product title and the first variant price, create a simple list or table displaying the product title and variant price, sorted by price (descending) - excluding any products where the first variant price is 0.00.
+- Refresh the list every 60 seconds.
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import React from "react"
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      info: [],
+    }
+  }
+
+  interval = null
+
+  componentDidMount = () =>{
+    this.interval = setInterval(this.getData, 60000)
+    this.getData()
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.interval)
+  }
+
+  getData = async () => {
+    const {data} = await axios.get("https://onedrop.today/products.json")
+    const {products} = data
+    const refreshedInfo = products.map((el) => {
+      let {title, variants, images} = el
+      let {price} = variants[0]
+      let [image] = images
+      let imageSrc = image.src
+      return {title, price, imageSrc}
+    }).filter((el) => {
+      return el.price !== "0.00"
+    }).sort((a, b) => {
+      return Number(b.price) - Number(a.price)
+    })
+    this.setState({info: refreshedInfo})
+  }
+
+  render = () => {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body">
+        {this.state.info.map((product, idx) =>
+        {
+          return (
+            <div className="card" key={idx}>
+              <p>{product.title}</p>
+              <p>${product.price}</p>
+              <img src={product.imageSrc} alt="product" width={50} />
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
